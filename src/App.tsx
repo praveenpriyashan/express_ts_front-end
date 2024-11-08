31b79e732242d1316369eaa02cb175a1051c5715
praveenpriyashan <praveenpriyashan@gmail.com>
2024-11-08 19:33:12 +0530

    display the notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {Note} from "./model/note";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [notes, setNotes] = useState<Note[]>([]);
+
+    useEffect(() => {
+        axios.get("/api/notes/")
+            .then((response) => {
+                setNotes(response.data);
+            })
+            .catch((err) => {
+                console.log(err);
+                alert("Failed to load notes");
+            })
+    }, []);
+    return (
+        <div className="App">
+            {
+                notes && notes.map((note: Note, index) => {
+                    return (
+                        <div key={index}>
+                            {note.title} - {note.text}
+                            <button type="button" className="btn btn-primary btn-sm">Edit</button>
+                            <button type="button" className="btn btn-danger btn-sm">Delete</button>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    );
 }
 
 export default App;
